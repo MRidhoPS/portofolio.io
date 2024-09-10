5ebcbca2b337c26d6cbacad36876b2b71aa165ad
MRidhoPS <mridhophs@gmail.com>
2024-09-10 22:16:21 +0700

    release 1.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3322832872"
+    serviceWorkerVersion: "2056321089"
   }
 });
